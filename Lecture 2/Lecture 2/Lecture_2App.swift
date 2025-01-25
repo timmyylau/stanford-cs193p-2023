30529ca89ec19cc4c36bbd833c42727fd4961fb6
Timmy Lau <tlau9898@gmail.com>
2025-01-25 15:58:51 -0800

    Lecture 2 done

---
+++ Lecture 2/Lecture 2/Lecture_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture_2App.swift
+//  Lecture 2
+//
+//  Created by Timmy Lau on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Lecture_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
